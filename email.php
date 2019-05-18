5316786899ec9df27c67c7112eeb4ec5086b4794
Victor <victorbucar@outlook.com>
2019-05-18 14:49:58 +0100

    message form

--- email.php
+++ email.php
@@ -1,6 +1,6 @@
 <?php
-    foreach ($_POST as $post_var)(
+    foreach ($_POST as $post_var){
         echo strtoupper($post_var) . "<br/>";
         echo "<script>console.log( 'Debug Objects: " . $post_var . "' );</script>";
-        )
+    }
 ?>
