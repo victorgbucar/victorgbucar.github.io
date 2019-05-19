0b8e1d4ef27c389144f14e77536fe21ec352ccf3
Victor <victorbucar@outlook.com>
2019-05-19 16:59:42 +0100

    remove php file add img

--- email.php
+++
@@ -1,6 +0,0 @@
-<?php
-    foreach ($_POST as $post_var){
-        echo strtoupper($post_var) . "<br/>";
-        echo "<script>console.log( 'Debug Objects: " . $post_var . "' );</script>";
-    }
-?>
